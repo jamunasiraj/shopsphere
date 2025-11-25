eab23d31d5477040c99c98e305e3f90184bfd860
Jamuna <jamunasiraj@gmail.com>
2025-11-25 11:13:08 +0100

    added FaviconSetter into MainLayout page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import FaviconSetter from "../components/FaviconSetter";
 
 const MainLayout = () => {
   return (
     <>
+      <FaviconSetter />
       <NavBar />
       <Outlet />
     </>
