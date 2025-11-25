38616e4c9b84041b3dd47847b23e177b4d7ba6a6
Jamuna <jamunasiraj@gmail.com>
2025-11-25 11:11:44 +0100

    added favicon ico

--- src/components/FaviconSetter.jsx
+++ src/components/FaviconSetter.jsx
@@ -1,5 +1,18 @@
+import React, { useEffect } from "react";
+import favicon from "../assets/images/favicon.ico";
+
 const FaviconSetter = () => {
-  return <div></div>;
+  useEffect(() => {
+    const link =
+      document.querySelector("link[rel*='icon']") ||
+      document.createElement("link");
+    link.type = "image/x-icon";
+    link.rel = "shortcut icon";
+    link.href = favicon;
+    document.getElementsByTagName("head")[0].appendChild(link);
+  }, []);
+
+  return null;
 };
 
 export default FaviconSetter;
