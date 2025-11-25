9ce78ee07f4b99560a5417435523a82bc2e17832
Jamuna <jamunasiraj@gmail.com>
2025-11-25 11:26:06 +0100

    modified index  page and favicon ico in feviconsetter component

--- src/components/FaviconSetter.jsx
+++ src/components/FaviconSetter.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import favicon from "../assets/images/favicon.ico";
+import favicon from "../assets/images/favicon_io/favicon.ico";
 
 const FaviconSetter = () => {
   useEffect(() => {
