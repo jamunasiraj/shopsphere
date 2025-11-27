f986db213d25bb17de7dcdb2328b116f528ca870
Jamuna <jamunasiraj@gmail.com>
2025-11-27 12:55:00 +0100

    Removed unwanted imports

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useParams, useLocation } from "react-router-dom";
 
 const ProductList = () => {
